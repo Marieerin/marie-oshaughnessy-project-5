eaa4db915ff88d710684d5c0b1f3579bf0359cf5
Marie Erin <m.oshaughnessy@outlook.com>
2020-05-03 22:07:26 -0400

    Updates

---
+++ precache-manifest.707e67cf55024df73f9d8a3e1a6b825e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80952b597a2b37d4e50661bd38861936",
+    "url": "/marie-oshaughnessy-project-5/index.html"
+  },
+  {
+    "revision": "bdebd62aa2c1f22f7adb",
+    "url": "/marie-oshaughnessy-project-5/static/css/main.f714364c.chunk.css"
+  },
+  {
+    "revision": "ea7ca3b534d3ffe12db7",
+    "url": "/marie-oshaughnessy-project-5/static/js/2.ff5e8808.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/marie-oshaughnessy-project-5/static/js/2.ff5e8808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdebd62aa2c1f22f7adb",
+    "url": "/marie-oshaughnessy-project-5/static/js/main.d082afee.chunk.js"
+  },
+  {
+    "revision": "db0fd9558fc0b1468aeb",
+    "url": "/marie-oshaughnessy-project-5/static/js/runtime-main.481d72ae.js"
+  }
+]);
